3467047b49ab2a1af9ffacbbbab89988970e826b
romancraig <jpiegaro@gmail.com>
2014-06-14 16:31:19 -0400

    Lsn 2.5 FINAL CODE for daysbetweenDate
    Final code with all procedures --Passed
    Lsn 2.5 complete (please look at previous code I wrote for the last 2-3
    procedures as I like mine better).

--- Lsn2-5-daysinmonth.py
+++ Lsn2-5-daysinmonth.py
@@ -23,3 +23,36 @@ def daysMonth(month,year):
 				return 29
 			return 28
 
+def nextDay(year, month, day):
+    """Simple version: assume every month has 30 days"""
+    fncday=daysMonth(month,year)
+    if day < fncday:
+        return year, month, day + 1
+    else:
+        if month == 12:
+            return year + 1, 1, 1
+        else:
+            return year, month + 1, 1
+        
+def dateIsBefore(year1, month1, day1, year2, month2, day2):
+    """Returns True if year1-month1-day1 is before year2-month2-day2. Otherwise, returns False."""
+    if year1 < year2:
+        return True
+    if year1 == year2:
+        if month1 < month2:
+            return True
+        if month1 == month2:
+            return day1 < day2
+    return False        
+
+def daysBetweenDates(year1, month1, day1, year2, month2, day2):
+    """Returns the number of days between year1/month1/day1
+       and year2/month2/day2. Assumes inputs are valid dates
+       in Gregorian calendar."""
+    # program defensively! Add an assertion if the input is not valid!
+    assert not dateIsBefore(year2, month2, day2, year1, month1, day1)
+    days = 0
+    while dateIsBefore(year1, month1, day1, year2, month2, day2):
+        year1, month1, day1 = nextDay(year1, month1, day1)
+        days += 1
+    return days
