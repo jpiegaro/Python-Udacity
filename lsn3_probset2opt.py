4c5c5abe5f73975d7c41efa5d983eafc4223b1c7
romancraig <jpiegaro@gmail.com>
2014-06-20 17:35:17 -0400

    Lsn 3 updated coding
    1)sudoku--incorrect code but learned quite a bit from the iterative
    process.
    2)asymmetric square answer--no assistance on code

---
+++ lsn3_probset2opt.py
@@ -0,0 +1,38 @@
+# Modify Problem Set 31's (Optional) Symmetric Square to return True 
+# if the given square is antisymmetric and False otherwise. 
+# An nxn square is called antisymmetric if A[i][j]=-A[j][i] 
+# for each i=0,1,...,n-1 and for each j=0,1,...,n-1.
+
+def antisymmetric(a):
+    n=len(a)
+    count=0
+    for i in range(n):
+        for j in range(n):
+            if a[i][j]==-a[j][i]:
+                count+=1
+    if count==n*n:
+        return True
+    return False
+
+
+# Test Cases:
+
+print antisymmetric([[0, 1, 2], 
+                     [-1, 0, 3], 
+                     [-2, -3, 0]])   
+#>>> True
+
+print antisymmetric([[0, 0, 0],
+                     [0, 0, 0],
+                     [0, 0, 0]])
+#>>> True
+
+print antisymmetric([[0, 1, 2], 
+                     [-1, 0, -2], 
+                     [2, 2,  3]])
+#>>> False
+
+print antisymmetric([[1, 2, 5],
+                     [0, 1, -9],
+                     [0, 0, 1]])
+#>>> False
--- lsn3_probset_sudoku.py
+++ lsn3_probset_sudoku.py
@@ -1,4 +1,5 @@
 def sudoku(a):
+	#***this procedure will not differentiate values from the input list (integer vs. non-integer).
 	n=len(a)
 	b=1
 	main=[]
@@ -6,3 +7,31 @@ def sudoku(a):
 		main.append(b)
 		b+=1
 	return main
+
+def transpose(a):
+	n=len(a)
+	trans=a
+	for i in range(n):
+		for j in range(n):
+			trans[i][j] = trans[j][i]
+	return trans
+	
+def check_sudoku(a):
+	test = sudoku(a)
+	for e in a:
+		if set(e)==set(test):
+			return True
+	for f in transpose(a):
+		if set(f)==set(test):
+			return True
+	return False
+		
+case1=[[0,1,2],[1,2,3],[2,3,1]]
+case2=[[0,1,2],[0,1,2],[0,1,2]]
+
+def intTest(a):  #this procedure needs to be reworked
+	test=sudoku(a) #problem with sudoku procedure not recognizing non-integer values.
+	for e in test:
+		if e not in [1,2,3,4,5,6,7,8,9]:
+			return False
+	return True
